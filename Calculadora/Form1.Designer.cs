bc5fd5f04a6b79434507ea65e3ce8b87f848678b
Mariano Diaz <marianod9797@gmail.com>
2024-07-02 00:20:42 -0300

    Agregar archivos de proyecto.

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSuma = new Button();
+            btn0 = new Button();
+            btn1 = new Button();
+            btn4 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn5 = new Button();
+            btn2 = new Button();
+            btnResta = new Button();
+            btn9 = new Button();
+            btn6 = new Button();
+            btn3 = new Button();
+            btnPunto = new Button();
+            btnMult = new Button();
+            button20 = new Button();
+            button22 = new Button();
+            btnDiv = new Button();
+            txtScreen = new TextBox();
+            btnBorrar = new Button();
+            btnSigno = new Button();
+            SuspendLayout();
+            // 
+            // btnSuma
+            // 
+            btnSuma.BackColor = Color.DarkOrange;
+            btnSuma.FlatStyle = FlatStyle.Flat;
+            btnSuma.Font = new Font("Sitka Text", 14.2499981F);
+            btnSuma.ForeColor = SystemColors.Control;
+            btnSuma.Location = new Point(165, 214);
+            btnSuma.Name = "btnSuma";
+            btnSuma.Size = new Size(42, 45);
+            btnSuma.TabIndex = 0;
+            btnSuma.Text = "+";
+            btnSuma.UseVisualStyleBackColor = false;
+            btnSuma.Click += btnSuma_Click;
+            // 
+            // btn0
+            // 
+            btn0.BackColor = SystemColors.WindowFrame;
+            btn0.FlatStyle = FlatStyle.Flat;
+            btn0.Font = new Font("Sitka Text", 14.2499981F);
+            btn0.ForeColor = SystemColors.ButtonFace;
+            btn0.Location = new Point(12, 265);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(93, 45);
+            btn0.TabIndex = 2;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = false;
+            btn0.Click += btn0_Click;
+            // 
+            // btn1
+            // 
+            btn1.BackColor = SystemColors.WindowFrame;
+            btn1.FlatStyle = FlatStyle.Flat;
+            btn1.Font = new Font("Sitka Text", 14.2499981F);
+            btn1.ForeColor = SystemColors.ButtonFace;
+            btn1.Location = new Point(12, 214);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(42, 45);
+            btn1.TabIndex = 3;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = false;
+            btn1.Click += btn1_Click;
+            // 
+            // btn4
+            // 
+            btn4.BackColor = SystemColors.WindowFrame;
+            btn4.FlatStyle = FlatStyle.Flat;
+            btn4.Font = new Font("Sitka Text", 14.2499981F);
+            btn4.ForeColor = SystemColors.ButtonFace;
+            btn4.Location = new Point(12, 163);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(42, 45);
+            btn4.TabIndex = 4;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = false;
+            btn4.Click += btn4_Click;
+            // 
+            // btn7
+            // 
+            btn7.BackColor = SystemColors.WindowFrame;
+            btn7.FlatStyle = FlatStyle.Flat;
+            btn7.Font = new Font("Sitka Text", 14.2499981F);
+            btn7.ForeColor = SystemColors.ButtonFace;
+            btn7.Location = new Point(12, 112);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(42, 45);
+            btn7.TabIndex = 5;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = false;
+            btn7.Click += button6_Click;
+            // 
+            // btn8
+            // 
+            btn8.BackColor = SystemColors.WindowFrame;
+            btn8.FlatStyle = FlatStyle.Flat;
+            btn8.Font = new Font("Sitka Text", 14.2499981F);
+            btn8.ForeColor = SystemColors.ButtonFace;
+            btn8.Location = new Point(63, 112);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(42, 45);
+            btn8.TabIndex = 11;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = false;
+            btn8.Click += button7_Click;
+            // 
+            // btn5
+            // 
+            btn5.BackColor = SystemColors.WindowFrame;
+            btn5.FlatStyle = FlatStyle.Flat;
+            btn5.Font = new Font("Sitka Text", 14.2499981F);
+            btn5.ForeColor = SystemColors.ButtonFace;
+            btn5.Location = new Point(63, 163);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(42, 45);
+            btn5.TabIndex = 10;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = false;
+            btn5.Click += btn5_Click;
+            // 
+            // btn2
+            // 
+            btn2.BackColor = SystemColors.WindowFrame;
+            btn2.FlatStyle = FlatStyle.Flat;
+            btn2.Font = new Font("Sitka Text", 14.2499981F);
+            btn2.ForeColor = SystemColors.ButtonFace;
+            btn2.Location = new Point(63, 214);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(42, 45);
+            btn2.TabIndex = 9;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = false;
+            btn2.Click += btn2_Click;
+            // 
+            // btnResta
+            // 
+            btnResta.BackColor = Color.DarkOrange;
+            btnResta.FlatStyle = FlatStyle.Flat;
+            btnResta.Font = new Font("Sitka Text", 14.2499981F);
+            btnResta.ForeColor = SystemColors.Control;
+            btnResta.Location = new Point(165, 163);
+            btnResta.Name = "btnResta";
+            btnResta.Size = new Size(42, 45);
+            btnResta.TabIndex = 6;
+            btnResta.Text = "-";
+            btnResta.UseVisualStyleBackColor = false;
+            btnResta.Click += btnResta_Click;
+            // 
+            // btn9
+            // 
+            btn9.BackColor = SystemColors.WindowFrame;
+            btn9.FlatStyle = FlatStyle.Flat;
+            btn9.Font = new Font("Sitka Text", 14.2499981F);
+            btn9.ForeColor = SystemColors.ButtonFace;
+            btn9.Location = new Point(114, 112);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(42, 45);
+            btn9.TabIndex = 17;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = false;
+            btn9.Click += btn9_Click;
+            // 
+            // btn6
+            // 
+            btn6.BackColor = SystemColors.WindowFrame;
+            btn6.FlatStyle = FlatStyle.Flat;
+            btn6.Font = new Font("Sitka Text", 14.2499981F);
+            btn6.ForeColor = SystemColors.ButtonFace;
+            btn6.Location = new Point(114, 163);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(42, 45);
+            btn6.TabIndex = 16;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = false;
+            btn6.Click += btn6_Click;
+            // 
+            // btn3
+            // 
+            btn3.BackColor = SystemColors.WindowFrame;
+            btn3.FlatStyle = FlatStyle.Flat;
+            btn3.Font = new Font("Sitka Text", 14.2499981F);
+            btn3.ForeColor = SystemColors.ButtonFace;
+            btn3.Location = new Point(114, 214);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(42, 45);
+            btn3.TabIndex = 15;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = false;
+            btn3.Click += btn3_Click;
+            // 
+            // btnPunto
+            // 
+            btnPunto.BackColor = SystemColors.WindowFrame;
+            btnPunto.FlatStyle = FlatStyle.Flat;
+            btnPunto.Font = new Font("Sitka Text", 14.2499981F);
+            btnPunto.ForeColor = SystemColors.Control;
+            btnPunto.Location = new Point(114, 265);
+            btnPunto.Name = "btnPunto";
+            btnPunto.Size = new Size(42, 45);
+            btnPunto.TabIndex = 14;
+            btnPunto.Text = ".";
+            btnPunto.UseVisualStyleBackColor = false;
+            // 
+            // btnMult
+            // 
+            btnMult.BackColor = Color.DarkOrange;
+            btnMult.FlatStyle = FlatStyle.Flat;
+            btnMult.Font = new Font("Sitka Text", 14.2499981F);
+            btnMult.ForeColor = SystemColors.Control;
+            btnMult.Location = new Point(165, 112);
+            btnMult.Name = "btnMult";
+            btnMult.Size = new Size(42, 45);
+            btnMult.TabIndex = 12;
+            btnMult.Text = "*";
+            btnMult.UseVisualStyleBackColor = false;
+            btnMult.Click += btnMult_Click;
+            // 
+            // button20
+            // 
+            button20.BackColor = Color.DarkGray;
+            button20.FlatStyle = FlatStyle.Flat;
+            button20.Font = new Font("Sitka Text", 14.2499981F);
+            button20.ForeColor = SystemColors.Control;
+            button20.Location = new Point(114, 61);
+            button20.Name = "button20";
+            button20.Size = new Size(42, 45);
+            button20.TabIndex = 22;
+            button20.Text = "←";
+            button20.UseVisualStyleBackColor = false;
+            button20.Click += button20_Click;
+            // 
+            // button22
+            // 
+            button22.BackColor = Color.DarkOrange;
+            button22.FlatStyle = FlatStyle.Flat;
+            button22.Font = new Font("Sitka Text", 14.2499981F);
+            button22.ForeColor = SystemColors.Control;
+            button22.Location = new Point(165, 265);
+            button22.Name = "button22";
+            button22.Size = new Size(42, 45);
+            button22.TabIndex = 20;
+            button22.Text = "=";
+            button22.UseVisualStyleBackColor = false;
+            button22.Click += button22_Click;
+            // 
+            // btnDiv
+            // 
+            btnDiv.BackColor = Color.DarkOrange;
+            btnDiv.FlatStyle = FlatStyle.Flat;
+            btnDiv.Font = new Font("Sitka Text", 14.2499981F);
+            btnDiv.ForeColor = SystemColors.Control;
+            btnDiv.Location = new Point(165, 61);
+            btnDiv.Name = "btnDiv";
+            btnDiv.Size = new Size(42, 45);
+            btnDiv.TabIndex = 18;
+            btnDiv.Text = "/";
+            btnDiv.UseVisualStyleBackColor = false;
+            btnDiv.Click += btnDiv_Click;
+            // 
+            // txtScreen
+            // 
+            txtScreen.BackColor = Color.Black;
+            txtScreen.BorderStyle = BorderStyle.FixedSingle;
+            txtScreen.Font = new Font("Segoe UI", 17F);
+            txtScreen.ForeColor = SystemColors.Window;
+            txtScreen.Location = new Point(12, 12);
+            txtScreen.Name = "txtScreen";
+            txtScreen.Size = new Size(195, 38);
+            txtScreen.TabIndex = 24;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.BackColor = Color.DarkGray;
+            btnBorrar.FlatStyle = FlatStyle.Flat;
+            btnBorrar.Font = new Font("Sitka Text", 14.2499981F);
+            btnBorrar.ForeColor = SystemColors.Control;
+            btnBorrar.Location = new Point(12, 61);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(42, 45);
+            btnBorrar.TabIndex = 25;
+            btnBorrar.Text = "CE";
+            btnBorrar.UseVisualStyleBackColor = false;
+            btnBorrar.Click += btnBorrar_Click;
+            // 
+            // btnSigno
+            // 
+            btnSigno.BackColor = Color.DarkGray;
+            btnSigno.FlatStyle = FlatStyle.Flat;
+            btnSigno.Font = new Font("Sitka Text", 14.2499981F);
+            btnSigno.ForeColor = SystemColors.Control;
+            btnSigno.Location = new Point(63, 61);
+            btnSigno.Name = "btnSigno";
+            btnSigno.Size = new Size(42, 45);
+            btnSigno.TabIndex = 26;
+            btnSigno.Text = "+/-";
+            btnSigno.UseVisualStyleBackColor = false;
+            btnSigno.Click += btnSigno_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(218, 328);
+            Controls.Add(btnSigno);
+            Controls.Add(btnBorrar);
+            Controls.Add(txtScreen);
+            Controls.Add(button20);
+            Controls.Add(button22);
+            Controls.Add(btnDiv);
+            Controls.Add(btn9);
+            Controls.Add(btn6);
+            Controls.Add(btn3);
+            Controls.Add(btnPunto);
+            Controls.Add(btnMult);
+            Controls.Add(btn8);
+            Controls.Add(btn5);
+            Controls.Add(btn2);
+            Controls.Add(btnResta);
+            Controls.Add(btn7);
+            Controls.Add(btn4);
+            Controls.Add(btn1);
+            Controls.Add(btn0);
+            Controls.Add(btnSuma);
+            Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSuma;
+        private Button btn0;
+        private Button btn1;
+        private Button btn4;
+        private Button btn7;
+        private Button btn8;
+        private Button btn5;
+        private Button btn2;
+        private Button btnResta;
+        private Button btn9;
+        private Button btn6;
+        private Button btn3;
+        private Button btnPunto;
+        private Button btnMult;
+        private Button button20;
+        private Button button22;
+        private Button btnDiv;
+        private TextBox txtScreen;
+        private Button btnBorrar;
+        private Button btnSigno;
+    }
+}
---
+++ Calculadora/Form1.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        double primero;
+        double segundo;
+        string operador;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Instancias de las clases
+        Clases.Suma obj = new Clases.Suma();
+        Clases.Resta obj2 = new Clases.Resta();
+        Clases.Multiplicacion obj3 = new Clases.Multiplicacion();
+        Clases.Division obj4 = new Clases.Division();
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtScreen.Text += "7";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            txtScreen.Text += "8";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtScreen.Text += "0";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            txtScreen.Text += "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtScreen.Text += "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtScreen.Text += "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtScreen.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtScreen.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtScreen.Text += "6";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtScreen.Text += "9";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtScreen.Text, out primero))
+            {
+                operador = "+";
+                txtScreen.Clear();
+            }
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtScreen.Text, out primero))
+            {
+                operador = "-";
+                txtScreen.Clear();
+            }
+            else
+            {
+                txtScreen.Text = "-";
+            }
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtScreen.Text, out primero))
+            {
+                operador = "*";
+                txtScreen.Clear();
+            }
+        }
+
+            private void btnDiv_Click(object sender, EventArgs e)
+        {
+            operador = "/";
+            primero = double.Parse(txtScreen.Text);
+            txtScreen.Clear();
+        }
+
+        private void btnSigno_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtScreen.Text, out double numero))
+            {
+                numero = -numero;
+                txtScreen.Text = numero.ToString();
+            }
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            segundo = double.Parse(txtScreen.Text);
+            double resultado = 0;
+
+            switch (operador)
+            {
+                case "+":
+                    resultado = obj.Sumar(primero, segundo);
+                    break;
+                case "-":
+                    resultado = obj2.Restar(primero, segundo);
+                    break;
+                case "*":
+                    resultado = obj3.Multiplicar(primero, segundo);
+                    break;
+                case "/":
+                    resultado = obj4.Dividir(primero, segundo);
+                    break;
+            }
+            txtScreen.Text = resultado.ToString();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtScreen.Clear();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            if (txtScreen.Text.Length == 1)
+                txtScreen.Text = "";
+            else
+                txtScreen.Text = txtScreen.Text.Substring(0, txtScreen.Text.Length - 1);
+        }
+    }
+}
